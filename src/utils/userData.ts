61e8e58f3eb0b90d89fc9a4f5735e84d38970934
Pedro Lisboa <lisboa.ti.mkt@gmail.com>
2023-05-24 02:27:49 -0300

    feat: create portfolio

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
-  facebookUser: "",
+  nameUser: "Pedro Lisboa",
+  githubUser: "lisboaseyth",
+  linkedinUser: "pedro-lisboa-a4b9a0235",
+  whatsappNumber: "+5588992710349",
+  emailUser: "pedrolisboadeveloper@gmail.com",
+  instagramUser: "pedrowslisboa",
+  facebookUser: "pedrowslisboa",
 };
